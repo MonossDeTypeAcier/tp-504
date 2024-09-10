aadc246a9e32b0b388e46a85e8297e32a3fbea3a
morelrob <robin.morel@univ-rouen.com>
2024-09-10 11:28:49 +0200

    fin première scéance tp1

--- tp1/prog1.py
+++ tp1/prog1.py
@@ -1,1 +1,8 @@
+import fonctions as f 
 print("Hello, World!")
+
+while(True):	
+	a=int(input("Inserer une valeur à monter en puissance: "))
+	b=int(input("A quelle puissance? : "))
+	y=f.puissance(a,b)
+	print(a, "à la puissance",b,"=", y)
