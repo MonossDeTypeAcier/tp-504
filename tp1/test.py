7b697e45fc0671f1607cfe35413f81c65cbeada1
morelrob <robin.morel@univ-rouen.com>
2024-09-12 09:07:45 +0200

    Ajout de fonctions test négative + exceptions

--- tp1/test.py
+++ tp1/test.py
@@ -1,5 +1,14 @@
 import pytest
 import fonctions as f
-def test_1 ( ) :
+def test_1():
 	assert f.puissance(2 ,3) == 8
 	assert f.puissance(2 ,2) == 4
+def test_2(): 
+	assert f.puissance(-2 ,3) == -8
+	assert f.puissance(-2 ,2) == 4
+	assert f.puissance(2 ,-2) == 0.25
+	try:
+		x = f.puissance(0,-2)
+		assert False
+	except ZeroDivisionError:
+		assert True
