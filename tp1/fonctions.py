b0a3081a88cba11e98758d4ac893fd85c93ad7cc
morelrob <robin.morel@univ-rouen.com>
2024-09-12 09:38:48 +0200

    Changement fonctions

--- tp1/fonctions.py
+++ tp1/fonctions.py
@@ -4,6 +4,9 @@ def puissance(a,b):
 		raise TypeError("Only integers are allowed")
 	if not type(b) is int:
 		raise TypeError("Only integers are allowed")
-	c=a**b
+	#c=a**b
+	c=a
+	while (i<b-1)
+		c=a*c
 	return c
 
