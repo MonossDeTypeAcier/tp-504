310c137027c2c493392090313d9ee1730b67a7e0
MonossDeTypeAcier <robin.morel@univ-rouen.fr>
2024-09-23 09:40:52 +0200

    Filldb fonctionnel

--- tp4/data.sql
+++ tp4/data.sql
@@ -1,6 +1,7 @@
-CREATE  DATABASE demosql;
+DROP DATABASE demosql;
+CREATE DATABASE demosql;
 USE demosql;
-CREATE TABLE myTable (id int AUTO_INCREMENT, name varchar('45') NOT NULL, PRIMARY KEY (id));
-INSERT INTO myTable (id, name) VALUES (NULL, 'bob')
-INSERT INTO myTable (id, name) VALUES (NULL, 'alice')
-INSERT INTO myTable (id, name) VALUES (NULL, 'john')
+CREATE TABLE myTable (id int AUTO_INCREMENT, name varchar(45) NOT NULL, PRIMARY KEY (id));
+INSERT INTO myTable (id, name) VALUES (NULL, 'bob');
+INSERT INTO myTable (id, name) VALUES (NULL, 'alice');
+INSERT INTO myTable (id, name) VALUES (NULL, 'john');
