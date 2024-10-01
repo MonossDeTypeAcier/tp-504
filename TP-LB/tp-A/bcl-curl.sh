adcf58a6e30abe01e0169ff2b2c8b28a450a9439
MonossDeTypeAcier <robin.morel@univ-rouen.fr>
2024-10-01 14:58:30 +0200

    Début 3.2

--- TP-LB/tp-A/bcl-curl.sh
+++ TP-LB/tp-A/bcl-curl.sh
@@ -1,12 +1,10 @@
-total=500
 echo Start > result.txt
 for ((i=1; i<=500 ; i++))
 do 
-    curl 127.0.0.1:83 >> result.txt
+    curl 127.0.0.1:83 -s >> result.txt
 done
 echo "end" >> result.txt
 echo "Nombre de Hello 1 :"
-x= cat result.txt | grep "Hello 1"| wc -l
-echo $x
+cat result.txt | grep "Hello 1"| wc -l
 echo "Nombre de Hello 2 :"
 cat result.txt | grep "Hello 2"| wc -l
