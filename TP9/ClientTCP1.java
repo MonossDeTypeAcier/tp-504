5743577e55cfb7b23f93dc6a691629a519ad9245
MonossDeTypeAcier <robin.morel@univ-rouen.fr>
2024-10-18 13:23:02 +0200

    3.1

--- TP9/ClientTCP1.java
+++ TP9/ClientTCP1.java
@@ -1,3 +1,13 @@
+import java.io.*;
+import java.net.*;
+
 public class ClientTCP1 {
-    
+    try{
+            Socket socket =new Socket("localhost", 2016)
+            DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+            dOut.writeUTF("Message test");
+            socket.close();
+        }
+        catch (Exception ex)
+        {System.out.println("Erreur");}
 }
